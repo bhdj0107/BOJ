2dc803f77c4c85a93b1792c77e8c3be8034c43ec
gloure <bhdj0107@gmail.com>
2019-10-02 01:08:38 +0900

    코드 완성_백준 채첨했더니 런타임 에러

--- 17135/17135.v1.py
+++ 17135/17135.v1.py
@@ -1,11 +1,20 @@
 N, M, D = map(int, input().split())
 field = []
 wave_end = []
+result = []
+maxi = 99999999
+s_total = 0
+
 for i in range(N):
 	field.append(list(map(int, input().split())))
-
+for i in range(N):
+	result.append(list(map(int, field[i])))
+for i in range(N):
+	s_total += sum(field[i])
 for x in range(M):
-	wave_end.append(field)
+	wave_end.append([])
+	for i in range(N):
+		wave_end[x].append(list(map(int, field[i])))
 	for inv_y in range(N):
 		y = N - inv_y - 1
 		check = 0
@@ -13,26 +22,33 @@ for x in range(M):
 			pointer_x = x - dist
 			pointer_y = y
 			for i in range(dist+dist+1):
+				if (pointer_x < 0):
+					pointer_x += 1
+					pointer_y -= 1
+					continue
 				if (wave_end[x][pointer_y][pointer_x] == 1):
 					wave_end[x][pointer_y][pointer_x] = 0
 					check = 1
 					break
 				if (i < dist and dist != 0):
 					pointer_x += 1
-					pointer_y += 1
-				elif (i >= dist and dist != 0):
-					pointer_x -= 1
 					pointer_y -= 1
+				elif (i >= dist and dist != 0):
+					pointer_x += 1
+					pointer_y += 1
 			if (check == 1):
 				break
-for i in range(M):
-	for j in range(N):
-		print(wave_end[i][j])
-	print("\n\n")
 
+for i in range(M-2):
+	for j in range(i+1,M-1):
+		for k in range(i+2,M):					
+			for x in range(M):
+				for y in range(N):
+					result[y][x] = wave_end[i][y][x] * wave_end[j][y][x] * wave_end[k][y][x]
+			total = 0
+			for l in range(N):
+				total += sum(result[l])
+			if (total < maxi):
+				maxi = total
 
-
-
-
-
-
+print(s_total - maxi)
