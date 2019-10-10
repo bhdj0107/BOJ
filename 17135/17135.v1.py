fa7d0892bd865b26d5c5fcef038234576897114f
gloure <bhdj0107@gmail.com>
2019-10-10 16:18:26 +0900

    로직 틀림

--- 17135/17135.v1.py
+++ 17135/17135.v1.py
@@ -6,51 +6,51 @@ maxi = 999999999
 s_total = 0
 
 for i in range(N):
-	field.append(list(map(int, input().split())))
+    field.append(list(map(int, input().split())))
 for i in range(N):
-	result.append(list(map(int, field[i])))
+    result.append(list(map(int, field[i])))
 for i in range(N):
-	s_total += sum(field[i])
+    s_total += sum(field[i])
 for x in range(M):
-	wave_end.append([])
-	for i in range(N):
-		wave_end[x].append(list(map(int, field[i])))
-	for inv_y in range(N):
-		y = N - inv_y - 1
-		check = 0
-		for dist in range(D):
-			pointer_x = x - dist
-			pointer_y = y
-			for i in range(dist+dist+1):
-				if (pointer_x < 0):
-					pointer_x += 1
-					pointer_y -= 1
-					continue
-				if (pointer_x >= M):
-					continue
-				if (pointer_y >= 0 and wave_end[x][pointer_y][pointer_x] == 1):
-					wave_end[x][pointer_y][pointer_x] = 0
-					check = 1
-					break
-				if (i < dist and dist != 0):
-					pointer_x += 1
-					pointer_y -= 1
-				elif (i >= dist and dist != 0):
-					pointer_x += 1
-					pointer_y += 1
-			if (check == 1):
-				break
+    wave_end.append([])
+    for i in range(N):
+        wave_end[x].append(list(map(int, field[i])))
+    for inv_y in range(N):
+        y = N - inv_y - 1
+        check = 0
+        for dist in range(D):
+            pointer_x = x - dist
+            pointer_y = y
+            for i in range(dist+dist+1):
+                if (pointer_x < 0):
+                    pointer_x += 1
+                    pointer_y -= 1
+                    continue
+                if (pointer_x >= M):
+                    continue
+                if (pointer_y >= 0 and wave_end[x][pointer_y][pointer_x] == 1):
+                    wave_end[x][pointer_y][pointer_x] = 0
+                    check = 1
+                    break
+                if (i < dist and dist != 0):
+                    pointer_x += 1
+                    pointer_y -= 1
+                elif (i >= dist and dist != 0):
+                    pointer_x += 1
+                    pointer_y += 1
+            if (check == 1):
+                break
 
 for i in range(M-2):
-	for j in range(i+1,M-1):
-		for k in range(i+2,M):					
-			for x in range(M):
-				for y in range(N):
-					result[y][x] = wave_end[i][y][x] * wave_end[j][y][x] * wave_end[k][y][x]
-			total = 0
-			for l in range(N):
-				total += sum(result[l])
-			if (total < maxi):
-				maxi = total
+    for j in range(i+1,M-1):
+        for k in range(i+2,M):                  
+            for x in range(M):
+                for y in range(N):
+                    result[y][x] = wave_end[i][y][x] * wave_end[j][y][x] * wave_end[k][y][x]
+            total = 0
+            for l in range(N):
+                total += sum(result[l])
+            if (total < maxi):
+                maxi = total
 
 print(s_total - maxi)
--- 17136/17136.v1.py
+++ 17136/17136.v1.py
@@ -1,4 +1,3 @@
-# main_field = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],[0, 1, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],[0, 0, 0, 1, 1, 1, 1, 0, 0, 0],[0, 0, 0, 0, 1, 1, 1, 0, 0, 0],[0,0,1,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 main_field = []
 for _ in range(10):
 	main_field.append(list(map(int, input().split())))
@@ -21,32 +20,32 @@ for i in range(1,10):
 		if (main_field[i][j] == 1):
 			xy_field[i][j][0] = xy_field[i][j-1][0] + 1
 			xy_field[i][j][1] = xy_field[i-1][j][1] + 1
-# for i in range(10):
-# 	print(xy_field[i])
-
 for inv_size in range(5):
 	size = 5 - inv_size
 	size_cnt = 5
 	for inv_i in range(10):
 		i = 9 - inv_i
-		if (i < size or size_cnt == 0):
+		if (size_cnt == 0):
 			break
 		for inv_j in range(10):
+			temp = True
 			j = 9 - inv_j
-			if (j < size or size_cnt == 0):
+			if (size_cnt == 0):
 				break
 			if (main_field[i][j] == 1):
-				if(xy_field[i][j][0] >= size and xy_field[i][j][1] >= size):
+				for k in range(size):
+					if (xy_field[i-k][j][0] < size):
+						temp = False
+				if(xy_field[i][j][0] >= size and xy_field[i][j][1] >= size and temp):
 					cnt += 1
 					size_cnt -= 1
-					for k in range(size+1):
-						for l in range(size+1):
+					for k in range(size):
+						for l in range(size):
 							main_field[i-k][j-l] = 0
 
 
 
 for i in range(10):
-	print(main_field[i])
 	all_sum += sum(main_field[i])
 if (all_sum == 0):
 	print(cnt)
