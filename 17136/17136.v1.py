ea74fc1969faa6147a47a8a5565e6d1122ce7753
gloure <bhdj0107@gmail.com>
2019-10-11 21:51:04 +0900

    알고리즘은 솔루션이랑 같은거 같은데, 풀리질 않음 ㅠ, 어디서 무한루프 되는지도 모르겠음

--- 17136/17136.v1.py
+++ 17136/17136.v1.py
@@ -1,53 +1,53 @@
-main_field = []
-for _ in range(10):
-	main_field.append(list(map(int, input().split())))
-xy_field = [[[0,0] for _ in range(10)] for _ in range(10)]
-cnt = 0
-all_sum = 0
+import copy
 
-if (main_field[0][0] == 1):
-	xy_field[0][0][0] = 1
-	xy_field[0][0][1] = 1
-for i in range(1, 10):
-	if (main_field[0][i] == 1):
-		xy_field[0][i][0] = xy_field[0][i-1][0] + 1
-		xy_field[0][i][1] = 1
-	if (main_field[i][0] == 1):
-		xy_field[i][0][1] = xy_field[i-1][0][1] + 1
-		xy_field[i][0][0] = 1
-for i in range(1,10):
-	for j in range(1,10):
-		if (main_field[i][j] == 1):
-			xy_field[i][j][0] = xy_field[i][j-1][0] + 1
-			xy_field[i][j][1] = xy_field[i-1][j][1] + 1
-for inv_size in range(5):
-	size = 5 - inv_size
-	size_cnt = 5
-	for inv_i in range(10):
-		i = 9 - inv_i
-		if (size_cnt == 0):
-			break
-		for inv_j in range(10):
-			temp = True
-			j = 9 - inv_j
-			if (size_cnt == 0):
-				break
-			if (main_field[i][j] == 1):
-				for k in range(size):
-					if (xy_field[i-k][j][0] < size):
-						temp = False
-				if(xy_field[i][j][0] >= size and xy_field[i][j][1] >= size and temp):
-					cnt += 1
-					size_cnt -= 1
-					for k in range(size):
-						for l in range(size):
-							main_field[i-k][j-l] = 0
-
-
-
+main_field = [list(map(int, input().split())) for _ in range(10)]
+size_cnt = [5, 5, 5, 5, 5]
+cnt = -1
+total_cnt = 0
 for i in range(10):
-	all_sum += sum(main_field[i])
-if (all_sum == 0):
-	print(cnt)
-else:
-	print(-1)
+    total_cnt += sum(main_field[i])
+
+def check_paper(size, x, y, field):
+    temp = 0
+    if x + size - 1 >= 10:
+        return False
+    for i in range(size):
+        temp += sum(field[y + i][x:x + size])
+    if temp == size * size:
+        return True
+    return False
+
+
+def dfs(px, py, previous_field, depth, total_cnt):
+    global cnt
+    if cnt != -1 and depth > cnt:
+        return
+    if py == 10 or total_cnt == 0:
+        if cnt == -1 or depth < cnt:
+            cnt = depth
+        return
+
+    if previous_field[py][px] == 1:
+        temp_field = copy.deepcopy(previous_field)
+        for size in range(1, 6):
+            if size_cnt[size - 1] == 0:
+                continue
+            if check_paper(size, px, py, temp_field):
+                total_cnt -= 1
+                size_cnt[size - 1] -= 1
+                for x in range(size):
+                    for y in range(size):
+                        temp_field[py+y][px+x] = 0
+            if px != 9:
+                dfs(px + 1, py, temp_field, depth + 1, total_cnt)
+            else:
+                dfs(0, py + 1, temp_field, depth + 1, total_cnt)
+    else:
+        if px != 9:
+            dfs(px + 1, py, previous_field, depth, total_cnt)
+        else:
+            dfs(0, py + 1, previous_field, depth, total_cnt)
+
+
+dfs(0, 0, main_field, 0, total_cnt)
+print(cnt)
