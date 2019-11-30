666d45d202b8a98c80fc9e7f67b54084a8dd3a98
gloure <bhdj0107@gmail.com>
2019-11-30 15:01:17 +0900

    깃 정리

--- 1547/1547.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-ball = [True, False, False]
-for _ in range(N):
-    a, b = map(int, input().split())
-    ball[a - 1], ball[b - 1] = ball[b - 1], ball[a - 1]
-
-for i in range(3):
-    if ball[i]:
-        print(i + 1)
